8e9f80e3b5c43d9d678c1c7fdade082ab22b1082
iamawesomecat <iamawesomecat2@gmail.com>
2024-01-04 19:35:08 -0800

    Added subsystems constant
    Added a boolean constant for the drivebase

--- src/main/java/frc/team2412/robot/Subsystems.java
+++ src/main/java/frc/team2412/robot/Subsystems.java
@@ -1,7 +1,10 @@
 package frc.team2412.robot;
 
 public class Subsystems {
-	public static class SubsystemConstants {}
+	public static class SubsystemConstants {
+		public static final boolean DRIVEBASE_ENABLED = false;
+	}
+
 
 	public Subsystems() {
 		// initialize subsystems here (wow thats wild)
