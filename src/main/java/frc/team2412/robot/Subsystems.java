d12f7ff9e65c4b76d05bcbc7e50e1e13276841a1
kirbt <91921906+kirbt@users.noreply.github.com>
2024-01-16 18:16:44 -0800

    hardware + subsystems stuff

--- src/main/java/frc/team2412/robot/Subsystems.java
+++ src/main/java/frc/team2412/robot/Subsystems.java
@@ -3,9 +3,18 @@ package frc.team2412.robot;
 public class Subsystems {
 	public static class SubsystemConstants {
 		public static final boolean DRIVEBASE_ENABLED = false;
+		public static final boolean APRILTAGS_ENABLED = false;
+		public static final boolean LIMELIGHT_ENABLED = false;
+		public static final boolean CLIMB_ENABLED = false;
+		public static final boolean SHOOTER_ENBABLED = false;
+		public static final boolean INTAKE_ENABLED = false;
+
 	}
+
+
 
 	public Subsystems() {
 		// initialize subsystems here (wow thats wild)
+		
 	}
 }
