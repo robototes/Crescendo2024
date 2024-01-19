53355211141fb143af62273ac8b01e09476c04e9
iamawesomecat <iamawesomecat2@gmail.com>
2024-01-18 19:36:22 -0800

    spotless

--- src/main/java/frc/team2412/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/team2412/robot/subsystems/ShooterSubsystem.java
@@ -1,22 +1,19 @@
 package frc.team2412.robot.subsystems;
 
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.team2412.robot.Hardware;
 
-import com.ctre.phoenix6.hardware.TalonFX;
-
-public class ShooterSubsystem extends SubsystemBase{
-    // CONSTANTS
-    //MOTOR SPEED VALUES
-    public static final double SPEAKER_SHOOT_SPEED = 0.8;
-    public static final double AMP_SHOOT_SPEED = 0.2;
-    public static final double ANGLE_CHANGE_SPEED = 0.15;
-    //MOTOR INSTANCE VARIBLES
-    public ShooterSubsystem(){
-        TalonFX SHOOTER_TOP_MOTOR = new TalonFX(Hardware.SHOOTER_TOP_MOTOR_ID);
-        TalonFX SHOOTER_BOTTOM_MOTOR = new TalonFX(Hardware.SHOOTER_BOTTOM_MOTOR_ID);
-        TalonFX SHOOTER_ANGLE_MOTOR = new TalonFX(Hardware.SHOOTER_ANGLE_MOTOR_ID);
-    }
-
+public class ShooterSubsystem extends SubsystemBase {
+	// CONSTANTS
+	// MOTOR SPEED VALUES
+	public static final double SPEAKER_SHOOT_SPEED = 0.8;
+	public static final double AMP_SHOOT_SPEED = 0.2;
+	public static final double ANGLE_CHANGE_SPEED = 0.15;
+	// MOTOR INSTANCE VARIBLES
+	public ShooterSubsystem() {
+		TalonFX SHOOTER_TOP_MOTOR = new TalonFX(Hardware.SHOOTER_TOP_MOTOR_ID);
+		TalonFX SHOOTER_BOTTOM_MOTOR = new TalonFX(Hardware.SHOOTER_BOTTOM_MOTOR_ID);
+		TalonFX SHOOTER_ANGLE_MOTOR = new TalonFX(Hardware.SHOOTER_ANGLE_MOTOR_ID);
+	}
 }
