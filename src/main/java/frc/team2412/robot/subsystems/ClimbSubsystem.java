69c11b352f9bb713f00794c21a03db8656bb83b2
Yumnah <s-zuberiy@bsd405.org>
2024-01-25 18:28:34 -0800

    Adding PID and troubleshooting
    Adding PID to climb, changed to CANSparkFlex

--- src/main/java/frc/team2412/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/team2412/robot/subsystems/ClimbSubsystem.java
@@ -5,40 +5,48 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.team2412.robot.Robot;
 
 import com.revrobotics.SparkMaxAbsoluteEncoder;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkFlex;
+import com.revrobotics.CANSparkFlexLowLevel.MotorType;
+import com.revrobotics.CANSparkFlex.IdleMode;
 
 public class ClimbSubsystem extends SubsystemBase{
     //public or private?
-    //  CONSTANT VARIABLES
+    //CONSTANT VARIABLES
         //speeds (fix later)
     private final double EXTEND_SPEED = 0.1;
     private final double RETRACT_SPEED = -0.1;
-    private final double MOTOR_SPEED = 0.15;
+    //private final double MOTOR_SPEED = 0.15;
         //PID extension values
         //PID retraction values
         //height of chain in encoder??? units
 
-    //  INSTANCE VARIABLES 
-    private final CANSparkMax motor1;
-    private final CANSparkMax motor2;
-    private final SparkMaxAbsoluteEncoder encoder;
+    //  INSTANCE VARIABLES
+    private final CANSparkFlex motorLeft;
+    private final CANSparkFlex motorRight;
+    private final SparkFlexAbsoluteEncoder encoderLeft;
+    private final SparkFlexAbsoluteEncoder encoderRight;
+    private final SparkPIDController motorLeftPIDController;
+    private final SparkPIDController motorRightPIDController;
 
     //encoder position
     public ClimbSubsystem() {
         //motor = new MOTOR_NAME (port number found in hardware);
-        this.motor1 = new motor(CLIMB_MOTOR_ONE, MotorType.kBrushless);
-        this.motor2= new motor(CLIMB_MOTOR_TWO,MotorType.kBrushless );
-        this.encoder1 = new motor1.getAbsoluteEncoder(Type.kDutyCycle);
-        this.encoder2 = new motor2.getAbsoluteEncoder(Type.kDutyCycle);
+        this.motorLeft = new motor(CLIMB_MOTOR_ONE, MotorType.kBrushless);
+        this.motorRight = new motor(CLIMB_MOTOR_TWO,MotorType.kBrushless );
+        this.encoderLeft = new motorLeft.getAbsoluteEncoder(Type.kDutyCycle);
+        this.encoderRight = new motorRight.getAbsoluteEncoder(Type.kDutyCycle);
 
+        //PID
+        this.motorLeftPIDController = motorLeft.getPIDController();
+        this.motorRightPIDController = motorRight.getPIDController();
+        motorLeftPIDController.setFeedbackDevice(encoderLeft);
+        motorRightPIDController.setFeedbackDevice(encoderRight);
 
         //motor configuration
-        //kBreak refers to motor idleness (completely stopped)
-        motor1.setIdleMode(IdleMode.kBrake);
-        motor2.setIdleMode(IdleMode.kBrake);
-        //set PID
+        //kBrake refers to motor idleness (completely stopped)
+        motorLeft.setIdleMode(IdleMode.kBrake);
+        motorRight.setIdleMode(IdleMode.kBrake);
+        
 
     }
     
@@ -46,48 +54,57 @@ public class ClimbSubsystem extends SubsystemBase{
     //start arm
     //stop arm
     //    stolen code omg
-    /*public void stopArm(boolean stop) {
-        if (stop) {
-            motor.stopMotor();
-            motor2.stopMotor();
-        } 
-    */
 
     //extend arm
     //retract arm
     //set motor position
     //get+set motor speed
+    //stop arm
     public double getSpeed() {
-        return motor1.get();
-        return motor2.get();
+        return motorLeft.get();
     }
 
     public void setMotorUp() {
-        setMotor(MOTOR_SPEED);
+        setMotor(EXTEND_SPEED);
     }
     
     public void setMotorDown() {
-        setMotor(-MOTOR_SPEED);
+        setMotor(-RETRACT_SPEED);
     }
     
     public void setMotor(double speed) {
-        motor1.set(speed);
-        motor2.set(speed);
+        motorLeft.set(speed);
+        motorRight.set(speed);
+    }
+
+    public void stopArm() {
+        setMotor(0);
     }
 
     //return encoder angle
-    public double encoderPosition() {
-        return motor1.getPosition();
-        return motor2.getPosition();
+    public double encoderLeftPosition() {
+        return motorLeft.getPosition();
+    }
+    
+    public double encoderRightPosition() {
+        return motorRight.getPosition();
     }
 
     //calculate height climbed
     //reset encoder to 0 if reset == true
     public void resetEncoder(boolean reset) {
         if (reset) {
-            motor1.setPositionConversionFactor(0);
-            motor2.setPositionConversionFactor(0);
+            motorLeft.setPositionConversionFactor(0);
+            motorRight.setPositionConversionFactor(0);
         }
+    }
+
+
+    //changes the spring in climb
+    //please change rotation variable later
+    public void climb(double rotations) {
+        motorLeftPIDController.setReference(rotations, ControlType.kPosition);
+        motorRightPIDController.setReference(rotations, ControlType.kPosition);
     }
     //if exceeding limit (limit switch functions)
     //possibly print out encoder position for good measure
