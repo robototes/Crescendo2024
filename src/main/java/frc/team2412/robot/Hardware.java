2d597958f478153c5a885844284b5e121475aa55
SMKettle <119903560+SMKettle@users.noreply.github.com>
2024-01-29 18:00:42 -0800

    updated Intake Subsystem and hardware brilliant
    because i forgot

--- src/main/java/frc/team2412/robot/Hardware.java
+++ src/main/java/frc/team2412/robot/Hardware.java
@@ -12,5 +12,11 @@ public class Hardware {
 	// shooter [30 - 39]
 
 	// intake [40 - 49]
+	public static final int intakeMotorLeftID = 40;
+	public static final int intakeMotorRightID = 41;
+	public static final int intakeMotorFrontID = 42;
+	public static final int intakeMotorBackID = 43;
 
+	public static final int conveyorBeltOneID = 45;
+	public static final int conveyorBeltTwoID = 46;
 }
--- src/main/java/frc/team2412/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/team2412/robot/subsystems/IntakeSubsystem.java
@@ -1,22 +1,64 @@
 package frc.team2412.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.team2412.robot.Hardware.*;
 
-import com.revrobotics.CANSparkLowLevel.MotorType;
-import com.revrobotics.CANSparkMax;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-public class IntakeSubsystem extends SubsystemBase {
+public class IntakeSubsystem extends SubsystemBase{
 	public static final double INTAKE_IN_SPEED = 0.3;
 	public static final double INTAKE_FAST_OUT_SPEED = -0.7;
 
-	IntakeSubsystem() {
-		CANSparkMax INTAKE_MOTOR_BACK = new CANSparkMax(INTAKE_MOTOR_BACK_ID, MotorType.kBrushless);
-		CANSparkMax INTAKE_MOTOR_RIGHT = new CANSparkMax(INTAKE_MOTOR_RIGHT_ID, MotorType.kBrushless);
-		CANSparkMax INTAKE_MOTOR_FRONT = new CANSparkMax(INTAKE_MOTOR_FRONT_ID, MotorType.kBrushless);
-		CANSparkMax INTAKE_MOTOR_LEFT = new CANSparkMax(INTAKE_MOTOR_LEFT_ID, MotorType.kBrushless);
+    CANSparkMax intakeMotorBack;
+    CANSparkMax intakeMotorRight;
+    CANSparkMax intakeMotorFront;
+    CANSparkMax intakeMotorLeft;
 
-		CANSparkMax CONVEYOR_BELT_ONE = new CANSparkMax(CONVEYOR_BELT_ONE_ID, MotorType.kBrushless);
-		CANSparkMax CONVEYOR_BELT_TWO = new CANSparkMax(CONVEYOR_BELT_TWO_ID, MotorType.kBrushless);
-	}
+    CANSparkMax conveyorBeltOne;
+    CANSparkMax conveyorBeltTwo;
+
+    public IntakeSubsystem() {
+
+        intakeMotorBack = new CANSparkMax(intakeMotorBackID, MotorType.kBrushless);
+        intakeMotorRight = new CANSparkMax(intakeMotorRightID, MotorType.kBrushless);
+        intakeMotorFront = new CANSparkMax(intakeMotorFrontID, MotorType.kBrushless);
+        intakeMotorLeft = new CANSparkMax(intakeMotorLeftID, MotorType.kBrushless);
+
+        conveyorBeltOne = new CANSparkMax(conveyorBeltOneID, MotorType.kBrushless);
+        conveyorBeltTwo = new CANSparkMax(conveyorBeltTwoID, MotorType.kBrushless);
+
+        intakeMotorBack.follow(intakeMotorFront);
+        intakeMotorLeft.follow(intakeMotorFront);
+        intakeMotorRight.follow(intakeMotorFront);
+
+    }
+    
+    public void intake(double speed, CANSparkMax motor){
+        motor.set(speed);
+    }
+
+    public void stopMotor(CANSparkMax motor){
+        motor.set(0);
+    }
+
+    public void spitOut(){
+        intakeMotorFront.set(INTAKE_FAST_OUT_SPEED);
+    }
+
+    
+    
+    /*
+    public void takeIn(double speed, CANSparkMax motor, CANSparkMax motor2){
+        motor.set(speed);
+        motor2.set(speed); 
+    }
+
+    public void takeIn(double speed, CANSparkMax motor, CANSparkMax motor2, CANSparkMax motor3){
+        motor.set(speed);
+        motor2.set(speed);
+        motor3.set(speed);
+    }*/
+
+
 }
