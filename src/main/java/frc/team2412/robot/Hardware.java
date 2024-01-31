203a203db4aa722f4cdb2ed8dd2f9792f57eac13
SMKettle <119903560+SMKettle@users.noreply.github.com>
2024-01-30 18:42:31 -0800

    New IntakeSubsystem based on changes
    gulp

--- src/main/java/frc/team2412/robot/Hardware.java
+++ src/main/java/frc/team2412/robot/Hardware.java
@@ -11,12 +11,22 @@ public class Hardware {
 
 	// shooter [30 - 39]
 
-	// intake [40 - 49]
-	public static final int intakeMotorLeftID = 40;
-	public static final int intakeMotorRightID = 41;
-	public static final int intakeMotorFrontID = 42;
-	public static final int intakeMotorBackID = 43;
+	// intake [40 - 60]
+	public static final int INTAKE_1 = 40;
+	public static final int INTAKE_2 = 41;
+	public static final int INTAKE_3 = 42;
+	public static final int INTAKE_4 = 43;
 
-	public static final int conveyorBeltOneID = 45;
-	public static final int conveyorBeltTwoID = 46;
+	public static final int INJEST_1 = 44;
+	public static final int INJEST_2 = 45;
+	public static final int INJEST_3 = 46;
+
+	public static final int INDEX_1 = 47;
+	public static final int INDEX_2 = 48;
+	public static final int INDEX_3 = 49;
+
+	public static final int FEEDER_1 = 50;
+	public static final int FEEDER_2 = 51;
+	public static final int FEEDER_3 = 52;
+	public static final int FEEDER_4 = 53;
 }
--- src/main/java/frc/team2412/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/team2412/robot/subsystems/IntakeSubsystem.java
@@ -1,64 +1,107 @@
 package frc.team2412.robot.subsystems;
 
+import com.revrobotics.CANSparkFlex;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.team2412.robot.Hardware.*;
 
-public class IntakeSubsystem extends SubsystemBase{
+public class IntakeSubsystem extends SubsystemBase {
 	public static final double INTAKE_IN_SPEED = 0.3;
-	public static final double INTAKE_FAST_OUT_SPEED = -0.7;
+	public static final double INGEST_SPEED = 0.2;
+    public static final double INDEX_SPEEED = 0.2;
+    public static final double FEEDER = 0.25; //YUMNAH please initialize THIS
+    public static final double DEEPS = 2.0;
 
-    CANSparkMax intakeMotorBack;
-    CANSparkMax intakeMotorRight;
-    CANSparkMax intakeMotorFront;
-    CANSparkMax intakeMotorLeft;
 
-    CANSparkMax conveyorBeltOne;
-    CANSparkMax conveyorBeltTwo;
+    //'skirt' intake
+    private final CANSparkFlex intakeMotor1;
+    private final CANSparkFlex intakeMotor2;
+    private final CANSparkFlex intakeMotor3;
+    private final CANSparkFlex intakeMotor4;
+
+    private final CANSparkFlex injestMotor1;
+    private final CANSparkFlex injestMotor2;
+    private final CANSparkFlex injestMotor3;
 
     public IntakeSubsystem() {
 
-        intakeMotorBack = new CANSparkMax(intakeMotorBackID, MotorType.kBrushless);
-        intakeMotorRight = new CANSparkMax(intakeMotorRightID, MotorType.kBrushless);
-        intakeMotorFront = new CANSparkMax(intakeMotorFrontID, MotorType.kBrushless);
-        intakeMotorLeft = new CANSparkMax(intakeMotorLeftID, MotorType.kBrushless);
+        intakeMotor1 = new CANSparkFlex(Hardware.INTAKE_1, MotorType.kBrushless);
+        intakeMotor2 = new CANSparkFlex(Hardware.INTAKE_2, MotorType.kBrushless);
+        intakeMotor3 = new CANSparkFlex(Hardware.INTAKE_3, MotorType.kBrushless);
+        intakeMotor4 = new CANSparkFlex(Hardware.INTAKE_4, MotorType.kBrushless);
 
-        conveyorBeltOne = new CANSparkMax(conveyorBeltOneID, MotorType.kBrushless);
-        conveyorBeltTwo = new CANSparkMax(conveyorBeltTwoID, MotorType.kBrushless);
+        resetMotors();
 
-        intakeMotorBack.follow(intakeMotorFront);
-        intakeMotorLeft.follow(intakeMotorFront);
-        intakeMotorRight.follow(intakeMotorFront);
+        injestMotor1 = new CANSparkFlex(Hardware.INJEST_1, MotorType.kBrushless);
+        injestMotor2 = new CANSparkFlex(Hardware.INJEST_2, MotorType.kBrushless);
+        injestMotor3 = new CANSparkFlex(Hardware.INJEST_3, MotorType.kBrushless);
 
+        intakeMotor2.follow(intakeMotor1);
+        intakeMotor3.follow(intakeMotor1);
+        intakeMotor4.follow(intakeMotor1);
     }
     
-    public void intake(double speed, CANSparkMax motor){
-        motor.set(speed);
+    public void resetMotors() {
+        intakeMotor1.restoreFactoryDefaults();
+        intakeMotor2.restoreFactoryDefaults();
+        intakeMotor3.restoreFactoryDefaults();
+        intakeMotor4.restoreFactoryDefaults();
+
+        injestMotor1.restoreFactoryDefaults();
+        injestMotor2.restoreFactoryDefaults();
+        injestMotor3.restoreFactoryDefaults();
+
+        intakeMotor1.setIdleMode(IdleMode.kBrake);
+        intakeMotor2.setIdleMode(IdleMode.kBrake);
+        intakeMotor3.setIdleMode(IdleMode.kBrake);
+        intakeMotor4.setIdleMode(IdleMode.kBrake);
+
+        injestMotor1.setIdleMode(IdleMode.kBrake);
+        injestMotor2.setIdleMode(IdleMode.kBrake);
+        injestMotor3.setIdleMode(IdleMode.kBrake);
+
+        intakeMotor1.setInverted(true);
+        intakeMotor2.setInverted(true);
+        intakeMotor3.setInverted(true);
+        intakeMotor4.setInverted(true);
+
+        intakeMotor1.setInverted(true);
+        intakeMotor2.setInverted(true);
+        intakeMotor3.setInverted(true);
+
+        intakeMotor1.setSmartCurrentLimit(20);
+        intakeMotor2.setSmartCurrentLimit(20);
+        intakeMotor3.setSmartCurrentLimit(20);
+        intakeMotor4.setSmartCurrentLimit(20);
+
+        injestMotor1.setSmartCurrentLimit(20);
+        injestMotor2.setSmartCurrentLimit(20);
+        injestMotor3.setSmartCurrentLimit(20);
+
+        intakeMotor1.burnFlash();
+        intakeMotor2.burnFlash();
+        intakeMotor3.burnFlash();
+        intakeMotor4.burnFlash();
+
+        injestMotor1.burnFlash();
+        injestMotor2.burnFlash();
+        injestMotor3.burnFlash();
+
     }
 
-    public void stopMotor(CANSparkMax motor){
-        motor.set(0);
+    public void intake() {
+        intakeMotor1.set(INTAKE_IN_SPEED);
     }
 
-    public void spitOut(){
-        intakeMotorFront.set(INTAKE_FAST_OUT_SPEED);
+    void stopMotor() {
+        intakeMotor1.set(0);
     }
 
-    
-    
-    /*
-    public void takeIn(double speed, CANSparkMax motor, CANSparkMax motor2){
-        motor.set(speed);
-        motor2.set(speed); 
+    public void spitOut() {
+        intakeMotor1.set(INTAKE_FAST_OUT_SPEED);
     }
-
-    public void takeIn(double speed, CANSparkMax motor, CANSparkMax motor2, CANSparkMax motor3){
-        motor.set(speed);
-        motor2.set(speed);
-        motor3.set(speed);
-    }*/
-
 
 }
