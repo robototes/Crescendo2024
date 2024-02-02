2e2d807119dacab959e08420fe56640826f09a04
iamawesomecat <iamawesomecat2@gmail.com>
2024-02-01 18:02:08 -0800

    launcher angle was set
    frfr

--- src/main/java/frc/team2412/robot/commands/launcher/SetLauncherAngle.java
+++ src/main/java/frc/team2412/robot/commands/launcher/SetLauncherAngle.java
@@ -4,11 +4,11 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.team2412.robot.subsystems.LauncherSubsystem;
 
-public class LauncherPreSetAngle extends Command {
+public class SetLauncherAngle extends Command {
 	private final LauncherSubsystem launcherSubsystem;
 	private final double launcherAngle;
 
-	public LauncherPreSetAngle(LauncherSubsystem launcherSubsystem, double angle) {
+	public SetLauncherAngle(LauncherSubsystem launcherSubsystem, double angle) {
 		this.launcherSubsystem = launcherSubsystem;
 		this.launcherAngle = angle;
 		addRequirements(launcherSubsystem);
