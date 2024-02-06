2b9fbf88632969b0ddb334d2b67764ba324d59a6
Yumnah <s-zuberiy@bsd405.org>
2024-02-05 18:57:46 -0800

    fixing github goofs
    Adding changes made that didnt commit properly

--- src/main/java/frc/team2412/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/team2412/robot/subsystems/IntakeSubsystem.java
@@ -5,11 +5,18 @@ import static frc.team2412.robot.Hardware.*;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkFlex;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.Map;
 
 public class IntakeSubsystem extends SubsystemBase {
 	public static final double INTAKE_OUT_SPEED = -0.7;
 	public static final double INTAKE_IN_SPEED = 0.3;
+	public static final double INGEST_SPEED = 0.2;
+	public static final double INDEX_SPEED = 0.2;
+	public static final double FEEDER_SPEED = 0.25;
 
 	public static final double INDEX_IN_SPEED = 0.3;
 	public static final double INDEX_OUT_SPEED = 0.3;
@@ -43,109 +50,78 @@ public class IntakeSubsystem extends SubsystemBase {
 		resetMotors();
 	}
 
+	// METHODS
+
+	// Configuring Motors
+	private void configureMotor(CANSparkFlex motor) {
+		motor.restoreFactoryDefaults();
+		motor.setIdleMode(IdleMode.kBrake);
+		motor.setInverted(true);
+		motor.setSmartCurrentLimit(20);
+		motor.burnFlash();
+	}
+
 	public void resetMotors() {
-		// reset
-		intakeMotorFront.restoreFactoryDefaults();
-		intakeMotorBack.restoreFactoryDefaults();
-		intakeMotorLeft.restoreFactoryDefaults();
-		intakeMotorRight.restoreFactoryDefaults();
+		configureMotor(intakeMotorFront);
+		configureMotor(intakeMotorBack);
+		configureMotor(intakeMotorLeft);
+		configureMotor(intakeMotorRight);
 
-		indexMotorTop.restoreFactoryDefaults();
-		indexMotorBottom.restoreFactoryDefaults();
+		configureMotor(indexMotorTop);
+		configureMotor(indexMotorBottom);
 
-		feederMotor.restoreFactoryDefaults();
-
-		// brake motors
-		intakeMotorFront.setIdleMode(IdleMode.kBrake);
-		intakeMotorBack.setIdleMode(IdleMode.kBrake);
-		intakeMotorLeft.setIdleMode(IdleMode.kBrake);
-		intakeMotorRight.setIdleMode(IdleMode.kBrake);
-
-		indexMotorTop.setIdleMode(IdleMode.kBrake);
-		indexMotorBottom.setIdleMode(IdleMode.kBrake);
-
-		feederMotor.setIdleMode(IdleMode.kBrake);
-
-		// Invert motors
-		intakeMotorFront.setInverted(true);
-		intakeMotorBack.setInverted(true);
-		intakeMotorLeft.setInverted(true);
-		intakeMotorRight.setInverted(true);
-
-		indexMotorTop.setInverted(true);
-		indexMotorBottom.setInverted(true);
-
-		// Limit voltage for motors
-		intakeMotorFront.setSmartCurrentLimit(20);
-		intakeMotorBack.setSmartCurrentLimit(20);
-		intakeMotorLeft.setSmartCurrentLimit(20);
-		intakeMotorRight.setSmartCurrentLimit(20);
-
-		indexMotorTop.setSmartCurrentLimit(20);
-		indexMotorBottom.setSmartCurrentLimit(20);
-
-		feederMotor.setSmartCurrentLimit(20);
-
-		// save motor settings even while motor dies
-		intakeMotorFront.burnFlash();
-		intakeMotorBack.burnFlash();
-		intakeMotorLeft.burnFlash();
-		intakeMotorRight.burnFlash();
-
-		indexMotorTop.burnFlash();
-		indexMotorBottom.burnFlash();
-
-		feederMotor.burnFlash();
+		configureMotor(feederMotor);
 	}
 
-	// intake methods
+	// MOTOR METHODS
+
+	// intake motor methods
+	private void intake(CANSparkFlex motor) {
+		motor.set(intakeSpeedEntry.getDouble(INTAKE_IN_SPEED));
+	}
+
+	private void index(CANSparkFlex motor) {
+		motor.set(indexSpeedEntry.getDouble(INTAKE_IN_SPEED));
+	}
+
+	private void feeder(CANSparkFlex motor) {
+		motor.set(feederSpeedEntry.getDouble(INTAKE_IN_SPEED));
+	}
+
 	public void intakeIn() {
-		intakeMotorFront.set(INTAKE_IN_SPEED);
-		intakeMotorLeft.set(INTAKE_IN_SPEED);
-		intakeMotorBack.set(INTAKE_IN_SPEED);
-		intakeMotorFront.set(INTAKE_IN_SPEED);
+		intake(intakeMotorFront);
+		intake(intakeMotorBack);
+		intake(intakeMotorLeft);
+		intake(intakeMotorRight);
 	}
 
-	public void intakeOut() {
-		intakeMotorFront.set(INTAKE_OUT_SPEED);
-		intakeMotorBack.set(INTAKE_OUT_SPEED);
-		intakeMotorLeft.set(INTAKE_OUT_SPEED);
-		intakeMotorRight.set(INTAKE_OUT_SPEED);
+	public void indexIn() {
+		index(indexMotorTop);
+		index(indexMotorBottom);
+	}
+
+	public void feederIn() {
+		feeder(feederMotor);
+	}
+
+	// Stop Motor methods
+	private void stopMotor(CANSparkFlex motor) {
+		motor.set(0);
 	}
 
 	public void stopIntakeMotors() {
-		intakeMotorFront.set(0);
-		intakeMotorBack.set(0);
-		intakeMotorLeft.set(0);
-		intakeMotorRight.set(0);
+		stopMotor(intakeMotorFront);
+		stopMotor(intakeMotorBack);
+		stopMotor(intakeMotorLeft);
+		stopMotor(intakeMotorRight);
 	}
 
-	// index methods
-	public void indexIn() {
-		indexMotorTop.set(INDEX_IN_SPEED);
-		indexMotorBottom.set(INDEX_IN_SPEED);
+	public void stopIndexMotor() {
+		stopMotor(indexMotorTop);
+		stopMotor(indexMotorBottom);
 	}
 
-	public void indexOut() {
-		indexMotorTop.set(INDEX_OUT_SPEED);
-		indexMotorBottom.set(INDEX_OUT_SPEED);
-	}
-
-	public void stopIndexMotors() {
-		indexMotorTop.set(0);
-		indexMotorBottom.set(0);
-	}
-
-	// feeder methods
-	public void feederIn() {
-		feederMotor.set(FEEDER_IN_SPEED);
-	}
-
-	public void feederOut() {
-		feederMotor.set(FEEDER_OUT_SPEED);
-	}
-
-	public void stopFeeder() {
-		feederMotor.set(0);
+	public void stopFeederMotor() {
+		stopMotor(feederMotor);
 	}
 }
