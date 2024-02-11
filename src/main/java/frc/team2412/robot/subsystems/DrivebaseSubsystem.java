e5c368a09a1b8b3777e73d2510d1ccd01e1cfc0a
DriverStation3 <58869582+jbko6@users.noreply.github.com>
2024-02-10 17:30:23 -0800

    Update configs for first tests

--- src/main/java/frc/team2412/robot/subsystems/DrivebaseSubsystem.java
+++ src/main/java/frc/team2412/robot/subsystems/DrivebaseSubsystem.java
@@ -130,7 +130,7 @@ public class DrivebaseSubsystem extends SubsystemBase {
 
 		// LOW verbosity only sends field position, HIGH sends full drive data, MACHINE sends data
 		// viewable by AdvantageScope
-		SwerveDriveTelemetry.verbosity = TelemetryVerbosity.LOW;
+		SwerveDriveTelemetry.verbosity = TelemetryVerbosity.MACHINE;
 	}
 
 	/**
