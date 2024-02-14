7882685f1905e35a135e422ca4c87d8fcc0917fb
Jetblackdragon <64041077+Jetblackdragon@users.noreply.github.com>
2024-02-13 17:44:42 -0800

    spotless

--- src/main/java/frc/team2412/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/team2412/robot/subsystems/LEDSubsystem.java
@@ -1,14 +1,15 @@
 package frc.team2412.robot.subsystems;
 
 import static frc.team2412.robot.Hardware.*;
+
 import edu.wpi.first.wpilibj.PWM;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LEDSubsystem extends SubsystemBase {
 	private static final double RED_LED_COLOR = 0.61;
-    private static final double GREEN_LED_COLOR = 0.77;
-    private static final double BLUE_LED_COLOR = 0.87;
-    private static final double YELLOW_LED_COLOR = 0.69;
+	private static final double GREEN_LED_COLOR = 0.77;
+	private static final double BLUE_LED_COLOR = 0.87;
+	private static final double YELLOW_LED_COLOR = 0.69;
 
 	private final PWM blinkin;
 
@@ -21,5 +22,4 @@ public class LEDSubsystem extends SubsystemBase {
 		blinkin.setSpeed(color);
 		System.out.println("set color " + color);
 	}
-
 }
