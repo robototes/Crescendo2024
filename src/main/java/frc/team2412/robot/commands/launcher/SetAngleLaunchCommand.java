ab8d60e0d1ea6d157387db88022e5afbdd028c35
iamawesomecat <iamawesomecat2@gmail.com>
2024-02-14 08:42:07 -0800

    removed some imports

--- src/main/java/frc/team2412/robot/commands/launcher/SetAngleLaunchCommand.java
+++ src/main/java/frc/team2412/robot/commands/launcher/SetAngleLaunchCommand.java
@@ -1,10 +1,7 @@
 package frc.team2412.robot.commands.launcher;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
+
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.team2412.robot.commands.launcher.SetAngleCommand;
-import frc.team2412.robot.commands.launcher.SetLaunchSpeedCommand;
-import frc.team2412.robot.commands.launcher.StopLauncherCommand;
 import frc.team2412.robot.subsystems.LauncherSubsystem;
 // this command can be used as a preset in controls, allowing the user to input a speed and angle
 // value when they keybind it multiple times.
