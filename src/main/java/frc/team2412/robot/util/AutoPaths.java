3c823fe96837fa00ba350d5d42e0d113c9f46226
Lucy <65467897+stewluj@users.noreply.github.com>
2024-02-20 20:05:54 -0800

    First Auto Condition

--- src/main/java/frc/team2412/robot/util/AutoPaths.java
+++ src/main/java/frc/team2412/robot/util/AutoPaths.java
@@ -1,17 +1,33 @@
 package frc.team2412.robot.util;
 
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public class AutoPaths {
 	public static SequentialCommandGroup topSpeakerAutoLineN1CenterLineN1N2 =
 			new SequentialCommandGroup(
 					AutoLogic.getAutonomousCommand("TopSpeakerQCenterLineN1"),
 					new ConditionalCommand(
-							AutoLogic.getAutonomousCommand("QCenterLineN1CenterLineN1"),
-							AutoLogic.getAutonomousCommand("QCenterLineN1CenterQLineN2"),
+							new SequentialCommandGroup(
+									AutoLogic.getAutonomousCommand("QCenterLineN1LCenterLineN1"),
+									AutoLogic.vibrateControllerCommand,
+									AutoLogic.getAutonomousCommand("LCenterN1QCenterLineN2"),
+									new ConditionalCommand(
+											new SequentialCommandGroup(
+													AutoLogic.getAutonomousCommand("QCenterLineN2LCenterLineN2")),
+											AutoLogic.getAutonomousCommand("QCenterLineN2LAutoLineN1"),
+											AutoLogic::dummyLogic)),
+							new SequentialCommandGroup(
+									AutoLogic.getAutonomousCommand("QCenterLineN1QCenterLineN2"),
+									new ConditionalCommand(
+											AutoLogic.getAutonomousCommand("QCenterLine2LCenterLine2"),
+											new InstantCommand(),
+											AutoLogic::dummyLogic),
+									AutoLogic.vibrateControllerCommand),
 							AutoLogic::dummyLogic),
-					AutoLogic.getAutonomousCommand("AutoLineN1"));
+					AutoLogic.vibrateControllerCommand);
 
 	public AutoPaths() {}
 }
