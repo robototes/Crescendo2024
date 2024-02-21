017e41b3ba39eaa8ff9e69d446912ef6ca8ef044
lucy <stewluj@outlook.com>
2024-02-20 20:19:01 -0800

    FirstLogicAuto

--- src/main/java/frc/team2412/robot/util/AutoPaths.java
+++ src/main/java/frc/team2412/robot/util/AutoPaths.java
@@ -3,10 +3,9 @@ package frc.team2412.robot.util;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public class AutoPaths {
-	public static SequentialCommandGroup topSpeakerAutoLineN1CenterLineN1N2 =
+	public static SequentialCommandGroup topSpeakerCenterLineN1N2AutoLine1 =
 			new SequentialCommandGroup(
 					AutoLogic.getAutonomousCommand("TopSpeakerQCenterLineN1"),
 					new ConditionalCommand(
@@ -16,18 +15,21 @@ public class AutoPaths {
 									AutoLogic.getAutonomousCommand("LCenterN1QCenterLineN2"),
 									new ConditionalCommand(
 											new SequentialCommandGroup(
-													AutoLogic.getAutonomousCommand("QCenterLineN2LCenterLineN2")),
+													AutoLogic.getAutonomousCommand("QCenterLineN2LCenterLineN2"),
+													AutoLogic.vibrateControllerCommand),
 											AutoLogic.getAutonomousCommand("QCenterLineN2LAutoLineN1"),
 											AutoLogic::dummyLogic)),
 							new SequentialCommandGroup(
 									AutoLogic.getAutonomousCommand("QCenterLineN1QCenterLineN2"),
 									new ConditionalCommand(
-											AutoLogic.getAutonomousCommand("QCenterLine2LCenterLine2"),
-											new InstantCommand(),
+											new SequentialCommandGroup(
+													AutoLogic.getAutonomousCommand("QCenterLineN2LCenterLineN2"),
+													AutoLogic.vibrateControllerCommand,
+													AutoLogic.getAutonomousCommand("LCenterLineN2LAutoLineN1")),
+											AutoLogic.getAutonomousCommand("QCenterLineN2LAutoLineN1"),
 											AutoLogic::dummyLogic),
 									AutoLogic.vibrateControllerCommand),
-							AutoLogic::dummyLogic),
-					AutoLogic.vibrateControllerCommand);
+							AutoLogic::dummyLogic));
 
 	public AutoPaths() {}
 }
