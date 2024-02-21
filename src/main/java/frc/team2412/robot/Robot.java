3299e1932a59eb9ed941d3cbe7d3996c51e6774c
iamawesomecat <iamawesomecat2@gmail.com>
2024-02-20 18:18:08 -0800

    spotless 🍕🍕🍕🍕🍕🍕🍕

--- src/main/java/frc/team2412/robot/Robot.java
+++ src/main/java/frc/team2412/robot/Robot.java
@@ -11,10 +11,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.team2412.robot.commands.diagnostic.IntakeDiagnosticCommand;
 import frc.team2412.robot.commands.diagnostic.diagnosticSequentialCommand;
-import frc.team2412.robot.subsystems.IntakeSubsystem;
-import frc.team2412.robot.subsystems.LauncherSubsystem;
 import frc.team2412.robot.util.MACAddress;
 import frc.team2412.robot.util.MatchDashboard;
 
@@ -104,8 +101,12 @@ public class Robot extends TimedRobot {
 
 	@Override
 	public void testInit() {
-		CommandScheduler.getInstance().schedule(new diagnosticSequentialCommand(subsystems.intakeSubsystem, subsystems.launcherSubsystem));
+		CommandScheduler.getInstance()
+				.schedule(
+						new diagnosticSequentialCommand(
+								subsystems.intakeSubsystem, subsystems.launcherSubsystem));
 	}
+
 	@Override
 	public void robotPeriodic() {
 		CommandScheduler.getInstance().run();
