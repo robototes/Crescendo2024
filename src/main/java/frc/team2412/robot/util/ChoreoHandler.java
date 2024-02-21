271e3b8514a7485b6334c1dc7d74068bffcd56e0
lucy <stewluj@outlook.com>
2024-02-20 20:21:40 -0800

    2 fixes

--- src/main/java/frc/team2412/robot/util/ChoreoHandler.java
+++ src/main/java/frc/team2412/robot/util/ChoreoHandler.java
@@ -19,10 +19,6 @@ public class ChoreoHandler {
 	private static final PIDController yController = new PIDController(0.1, 0, 0);
 	private static final PIDController rotationController = new PIDController(0.5, 0, 0);
 
-	// constructor
-
-	public ChoreoHandler() {}
-
 	// methods
 
 	private static boolean mirrorTrajectory() {
