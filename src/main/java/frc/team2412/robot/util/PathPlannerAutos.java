0f6f45446b569b3d05da29f53811e95dddf04794
Joseph Eng <91924258+KangarooKoala@users.noreply.github.com>
2024-02-24 11:03:52 -0800

    Switch to using jackson JSON parser

--- src/main/java/frc/team2412/robot/util/PathPlannerAutos.java
+++ src/main/java/frc/team2412/robot/util/PathPlannerAutos.java
@@ -1,5 +1,7 @@
 package frc.team2412.robot.util;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pathplanner.lib.path.PathPlannerPath;
 import com.pathplanner.lib.path.PathPlannerTrajectory;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -8,7 +10,6 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -16,51 +17,6 @@ import java.util.Map;
 import java.util.function.Function;
 
 public class PathPlannerAutos {
-	// Parsing utils
-
-	private static int indexOfEnd(String text, String target, int fromIndex) {
-		int i = text.indexOf(target, fromIndex);
-		if (i < 0) {
-			return i;
-		}
-		return i + target.length();
-	}
-
-	private static int skipWhitespace(String text, int i) {
-		while (Character.isWhitespace(text.charAt(i))) {
-			++i;
-		}
-		return i;
-	}
-
-	private static String readJsonString(String text, int quoteStart) {
-		int stringStart = quoteStart + 1;
-		int stringEnd = text.indexOf("\"", stringStart);
-		return text.substring(stringStart, stringEnd);
-	}
-
-	private static String readJsonValue(String text, int start) {
-		start = skipWhitespace(text, start);
-		int comma = text.indexOf(",", start);
-		int objectEnd = text.indexOf("}", start);
-		if (comma < 0) {
-			return text.substring(start, objectEnd);
-		}
-		if (objectEnd < 0) {
-			return text.substring(start, comma);
-		}
-		return text.substring(start, Math.min(comma, objectEnd));
-	}
-
-	private static boolean readJsonBoolean(String text, int start) {
-		// False for values that are not "true" or "false" (case insensitive)
-		return Boolean.parseBoolean(readJsonValue(text, start));
-	}
-
-	private static double readJsonDouble(String text, int start) throws NumberFormatException {
-		return Double.parseDouble(readJsonValue(text, start));
-	}
-
 	// Individual paths
 
 	private static PathPlannerPath loadPathPlannerPath(String name) {
@@ -84,79 +40,37 @@ public class PathPlannerAutos {
 
 	// Whole autos
 
-	private static boolean isChoreoAuto(String autoJson) {
-		int boolStart = skipWhitespace(autoJson, indexOfEnd(autoJson, "\"choreoAuto\":", -1));
-		return readJsonBoolean(autoJson, boolStart);
-	}
-
-	private static int getNextPathNameIndex(String autoJson, int start) {
-		while (true) {
-			// Find next occurrence of "type":
-			start = indexOfEnd(autoJson, "\"type\":", start);
-			if (start < 0) {
+	private static void addPathsFromCommand(
+			JsonNode commandJson, boolean isChoreo, List<PathPlannerPath> paths) {
+		String type = commandJson.get("type").asText();
+		switch (type) {
+			case "wait":
 				break;
-			}
-			int i = skipWhitespace(autoJson, start);
-			// Skip the command if it's not a path command
-			if (!autoJson.startsWith("\"path\"", i)) {
-				continue;
-			}
-			// Find start of the JSON object representing the command
-			int objectStart = autoJson.lastIndexOf("{", start);
-			// Find pathName within the command
-			int pathNameIndex = indexOfEnd(autoJson, "\"pathName\":", objectStart);
-			// Skip whitespace
-			pathNameIndex = skipWhitespace(autoJson, pathNameIndex);
-			// Double check we have a string
-			if (autoJson.charAt(pathNameIndex) != '"') {
-				continue;
-			}
-			// Done
-			return pathNameIndex;
+			case "named":
+				// TODO Handle conditional paths (after those are added)
+				break;
+			case "path":
+				String pathName = commandJson.get("data").get("pathName").asText();
+				paths.add(isChoreo ? getChoreoTrajectory(pathName) : getPathPlannerPath(pathName));
+				break;
+			case "sequential", "parallel", "race", "deadline":
+				for (JsonNode child : commandJson.get("data").get("commands")) {
+					addPathsFromCommand(child, isChoreo, paths);
+				}
+				break;
 		}
-		return -1;
 	}
 
-	private static List<PathPlannerPath> getPaths(String autoJson) {
+	private static List<PathPlannerPath> getPaths(JsonNode autoJson) {
+		JsonNode commandRoot = autoJson.get("command");
+		boolean isChoreo = autoJson.get("choreoAuto").asBoolean();
 		List<PathPlannerPath> paths = new ArrayList<>();
-		boolean isChoreo = isChoreoAuto(autoJson);
-		int start = -1;
-		while (true) {
-			int pathNameStart = getNextPathNameIndex(autoJson, start);
-			start = pathNameStart;
-			if (pathNameStart < 0) {
-				break;
-			}
-			String pathName = readJsonString(autoJson, pathNameStart);
-			if (isChoreo) {
-				paths.add(getChoreoTrajectory(pathName));
-			} else {
-				paths.add(getPathPlannerPath(pathName));
-			}
-		}
+		addPathsFromCommand(commandRoot, isChoreo, paths);
 		return paths;
 	}
 
-	private static Rotation2d getStartingRotation(String autoJson) {
-		int startingPoseIndex = indexOfEnd(autoJson, "\"startingPose\":", -1);
-		if (startingPoseIndex < 0) {
-			return null;
-		}
-		int poseObjectStart = skipWhitespace(autoJson, startingPoseIndex);
-		if (!autoJson.startsWith("{", poseObjectStart)) {
-			return null;
-		}
-		int rotationStart = indexOfEnd(autoJson, "\"rotation\":", poseObjectStart);
-		if (rotationStart < 0) {
-			return null;
-		}
-		double rotation;
-		try {
-			rotation = readJsonDouble(autoJson, skipWhitespace(autoJson, rotationStart));
-		} catch (NumberFormatException e) {
-			return null;
-		}
-		return Rotation2d.fromDegrees(rotation);
+	private static Rotation2d getStartingRotation(JsonNode autoJson) {
+		return Rotation2d.fromDegrees(autoJson.get("startingPose").get("rotation").asDouble());
 	}
 
 	private static ChassisSpeeds speedsFromState(PathPlannerTrajectory.State state) {
@@ -174,15 +88,15 @@ public class PathPlannerAutos {
 					"Attempted to load non-existent auto \"" + autoName + "\"", false);
 			return List.of();
 		}
-		String text;
+		JsonNode autoJson;
 		try {
-			text = Files.readString(autoFile.toPath());
+			autoJson = new ObjectMapper().readTree(autoFile);
 		} catch (IOException e) {
 			DriverStation.reportWarning("Could not load auto \"" + autoName + "\"", e.getStackTrace());
 			return List.of();
 		}
-		List<PathPlannerPath> paths = getPaths(text);
-		Rotation2d startingRotation = getStartingRotation(text);
+		List<PathPlannerPath> paths = getPaths(autoJson);
+		Rotation2d startingRotation = getStartingRotation(autoJson);
 		ChassisSpeeds startingSpeeds = new ChassisSpeeds();
 		List<PathPlannerTrajectory> trajectories = new ArrayList<>(paths.size());
 		for (var path : paths) {
