6ae0ef5fac9eec22bfbb51d0fb9494998f3dadc2
kirbt <91921906+kirbt@users.noreply.github.com>
2024-03-14 12:35:29 -0700

    start position within tolerance code

--- src/main/java/frc/team2412/robot/util/auto/AutoPath.java
+++ src/main/java/frc/team2412/robot/util/auto/AutoPath.java
@@ -2,6 +2,7 @@ package frc.team2412.robot.util.auto;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.commands.PathPlannerAuto;
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.team2412.robot.util.auto.AutoLogic.StartPosition;
@@ -20,8 +21,10 @@ public class AutoPath {
 		autoCommand = AutoBuilder.buildAuto(pathPlannerAutoName);
 		this.vision = vision;
 
+		// in the case that the auto for whatever reason's starting pose is slightly off,
+		// is still able to match with a startPosition if it is close enough
 		for (StartPosition pos : StartPosition.values()) {
-			if (startPose2d.equals(pos.startPose)) {
+			if (matchesStartPosition(pos)) {
 				startPosition = pos;
 				return;
 			}
@@ -51,5 +54,21 @@ public class AutoPath {
 
 	public boolean isVision() {
 		return vision;
+	}
+
+	/**
+	 * Checks the x, y, and rotation of the auto's starting position and compares it with the expected
+	 * starting position, returning true if it is considered close enough to be the same.
+	 *
+	 * @param expectedStartPosition
+	 * @return if it is matching
+	 */
+	public boolean matchesStartPosition(StartPosition expectedStartPosition) {
+		return (MathUtil.isNear(expectedStartPosition.startPose.getX(), startPose2d.getX(), .05)
+				&& MathUtil.isNear(expectedStartPosition.startPose.getY(), startPose2d.getY(), .05)
+				&& MathUtil.isNear(
+						expectedStartPosition.startPose.getRotation().getDegrees(),
+						startPose2d.getRotation().getDegrees(),
+						5));
 	}
 }
