36082af0a3877c49dd13d1c8acbbeb1728216836
Jetblackdragon <64041077+Jetblackdragon@users.noreply.github.com>
2024-03-14 18:50:03 -0700

    Changed deleted motors
    Fixed methods that used outdated motors

--- src/main/java/frc/team2412/robot/commands/LED/LightsCommand.java
+++ src/main/java/frc/team2412/robot/commands/LED/LightsCommand.java
@@ -29,7 +29,7 @@ public class LightsCommand extends Command {
 	public void execute() {
 		if (launcherSubsystem.isAtSpeed()) { // Checks if launcher is ready
 			ledSubsystem.setGREEN_LED();
-		} else if (intakeSubsystem.getIndexSensor()) { // Checks if note is in feeder
+		} else if (intakeSubsystem.indexSensorHasNote()) { // Checks if note is in feeder
 			ledSubsystem.setBLUE_LED();
 		} else if (intakeSubsystem.isIntakeOn()) { // Checks if intake is on
 			ledSubsystem.setYELLOW_LED();
--- src/main/java/frc/team2412/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/team2412/robot/subsystems/IntakeSubsystem.java
@@ -244,7 +244,7 @@ public class IntakeSubsystem extends SubsystemBase {
 	public boolean isIntakeOn() {
 		return (intakeMotorFront.get() != 0
 				|| indexMotorUpper.get() != 0
-				|| indexMotorLower.get() != 0
+				|| ingestMotor.get() != 0
 				|| feederMotor.get() != 0);
 	}
 
