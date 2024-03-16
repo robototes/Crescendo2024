ad50df222c195f54f793b6e0e7f19a2d327e351d
kirby <s-voki@bsd405.org>
2024-03-16 13:32:33 -0700

    gulp

--- src/main/java/frc/team2412/robot/Robot.java
+++ src/main/java/frc/team2412/robot/Robot.java
@@ -1,7 +1,6 @@
 package frc.team2412.robot;
 
 import com.ctre.phoenix6.SignalLogger;
-import com.pathplanner.lib.auto.AutoBuilder;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.PowerDistribution;
