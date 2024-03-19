1837d37219e5734a4e96f2de242705f1a366c9f1
Jetblackdragon <64041077+Jetblackdragon@users.noreply.github.com>
2024-03-18 18:05:59 -0700

    Changed isIntakeOn
    Changed isIntakeOn method to only check for intaking and not also rejecting

--- src/main/java/frc/team2412/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/team2412/robot/subsystems/IntakeSubsystem.java
@@ -242,10 +242,10 @@ public class IntakeSubsystem extends SubsystemBase {
 	}
 
 	public boolean isIntakeOn() {
-		return (intakeMotorFront.get() != 0
-				|| indexMotorUpper.get() != 0
-				|| ingestMotor.get() != 0
-				|| feederMotor.get() != 0);
+		return (intakeMotorFront.get() > 0
+				|| indexMotorUpper.get() > 0
+				|| ingestMotor.get() > 0
+				|| feederMotor.get() > 0);
 	}
 
 	@Override
