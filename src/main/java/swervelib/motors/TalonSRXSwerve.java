be717d29e95c6e82da70e37f5c7f51e9df9040d2
jbko6 <58869582+jbko6@users.noreply.github.com>
2024-04-14 21:55:09 -0700

    spotless

--- src/main/java/swervelib/motors/TalonSRXSwerve.java
+++ src/main/java/swervelib/motors/TalonSRXSwerve.java
@@ -5,7 +5,6 @@ import com.ctre.phoenix.motorcontrol.DemandType;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
-import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.can.TalonSRXConfiguration;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.Timer;
