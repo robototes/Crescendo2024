030408a9ff9056a9a494bedcb6b8fecc35d583b3
kirbt <91921906+kirbt@users.noreply.github.com>
2024-04-23 23:04:37 -0700

    debug

--- src/main/java/frc/team2412/robot/subsystems/AutonomousTeleopSubsystem.java
+++ src/main/java/frc/team2412/robot/subsystems/AutonomousTeleopSubsystem.java
@@ -1,13 +1,11 @@
 package frc.team2412.robot.subsystems;
 
-import java.util.List;
-
+import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.path.GoalEndState;
 import com.pathplanner.lib.path.PathConstraints;
 import com.pathplanner.lib.path.PathPlannerPath;
 import com.pathplanner.lib.pathfinding.LocalADStar;
 import com.pathplanner.lib.pathfinding.Pathfinding;
-
 import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -20,10 +18,12 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.team2412.robot.Robot;
 import frc.team2412.robot.Subsystems;
+import frc.team2412.robot.commands.drivebase.DriveToNoteCommand;
 import frc.team2412.robot.commands.intake.AllInCommand;
 import frc.team2412.robot.commands.launcher.FullTargetCommand;
 import frc.team2412.robot.commands.launcher.SetAngleAmpLaunchCommand;
 import frc.team2412.robot.subsystems.AutonomousTeleopSubsystem.RobotState;
+import java.util.List;
 
 public class AutonomousTeleopSubsystem extends SubsystemBase {
 
@@ -135,7 +135,7 @@ public class AutonomousTeleopSubsystem extends SubsystemBase {
 		},
 		SEARCHING {
 			AllInCommand intakeCommand;
-            boolean roamed;
+			boolean roamed;
 
 			@Override
 			public RobotState nextState() {
@@ -156,11 +156,10 @@ public class AutonomousTeleopSubsystem extends SubsystemBase {
 					currentCommand =
 							new DriveToNoteCommand(input.s.drivebaseSubsystem, input.s.limelightSubsystem);
 					currentCommand.schedule();
-				} else if (currentCommand != null && currentCommand.isFinished() {
-					if (roamed) {
-
-                    }
-                    currentCommand = 
+				} else if (currentCommand != null && currentCommand.isFinished()) {
+					if (roamed) {}
+					// currentCommand =
+					// TODO: idk what is being done here jbear
 				}
 				return this;
 			}
@@ -322,15 +321,15 @@ public class AutonomousTeleopSubsystem extends SubsystemBase {
 
 		Translation2d currentPosition = s.drivebaseSubsystem.getPose().getTranslation();
 
-		double collisionAngle = 0; // TODO: get angle of collision (degrees)
+		double collisionAngle = Math.tan(collisionDirection.getY() / collisionDirection.getX());
 
 		Translation2d collidingBotPosition =
 				new Translation2d(
 						currentPosition.getX() + (COLLISION_DISTANCE * Math.cos(collisionAngle)),
 						currentPosition.getY() + (COLLISION_DISTANCE * Math.sin(collisionAngle)));
 
-        // collision box x/y size varies on direction of acceleration
-        // im having an anuerysmn naming things (hardest part) lol
+		// collision box x/y size varies on direction of acceleration
+		// im having an anuerysmn naming things (hardest part) lol
 		double obstacleLengthX = COLLISION_DISTANCE;
 		double obstacleLengthY = COLLISION_DISTANCE;
 
@@ -348,6 +347,8 @@ public class AutonomousTeleopSubsystem extends SubsystemBase {
 						collidingBotPosition.plus(new Translation2d(-obstacleLengthX, -obstacleLengthY)));
 
 		Pathfinding.setDynamicObstacles(List.of(collisionBox), currentPosition);
+		// TODO: reset dynamic obstacles after certain condition
+		// After certain amount of time?
 	}
 
 	public void clearCollisionSpots() {
@@ -368,9 +369,9 @@ public class AutonomousTeleopSubsystem extends SubsystemBase {
 				new GoalEndState(0, sourcePose.getRotation()));
 	}
 
-    private PathPlannerPath getPathToSourceRoam(Translation2d robotPosition) {
-        return null;
-    }
+	private PathPlannerPath getPathToSourceRoam(Translation2d robotPosition) {
+		return null;
+	}
 
 	private PathPlannerPath getPathToSpeaker(Translation2d robotPosition) {
 		return null;
